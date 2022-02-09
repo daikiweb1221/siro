bbf5bca49090bbfefa440e09d72dedb5d1b22cce
daikiweb1221 <daiki.web1221@gmail.com>
2022-02-10 08:09:00 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
-  var topBtn = $('.pagetop');
+  var topBtn = $(".pagetop");
   topBtn.hide();
 
   // ボタンの表示設定
@@ -17,9 +17,13 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
   // ボタンをクリックしたらスクロールして上に戻る
   topBtn.click(function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 300, 'swing');
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      300,
+      "swing"
+    );
     return false;
   });
 
@@ -30,32 +34,40 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     $(".js-drawer-open").toggleClass("open");
     $(".drawer-menu").toggleClass("open");
     $("html").toggleClass("is-fixed");
-
   });
-
-
 
   // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
 
-  $(document).on('click', 'a[href*="#"]', function () {
+  $(document).on("click", 'a[href*="#"]', function () {
     let time = 400;
-    let header = $('header').innerHeight();
+    let header = $("header").innerHeight();
     let target = $(this.hash);
     if (!target.length) return;
     let targetY = target.offset().top - header;
-    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
+    $("html,body").animate({ scrollTop: targetY }, time, "swing");
     return false;
   });
 
   // ハンバーガー
-  $(".js-humburger").click(function () {//ボタンがクリックされたら
-    $(this).toggleClass('js-active');//ボタン自身に activeクラスを付与し
-      $(".js-gnav").toggleClass('js-panelactive');//ナビゲーションにpanelactiveクラスを付与
-  });
-  
-  $(".p-gnav a").click(function () {//ナビゲーションのリンクがクリックされたら
-      $(".js-humburger").removeClass('js-active');//ボタンの activeクラスを除去し
-      $(".js-gnav").removeClass('js-panelactive');//ナビゲーションのpanelactiveクラスも除去
+  $(".js-humburger").click(function () {
+    //ボタンがクリックされたら
+    $(this).toggleClass("js-active"); //ボタン自身に activeクラスを付与し
+    $(".js-gnav").toggleClass("js-panelactive"); //ナビゲーションにpanelactiveクラスを付与
   });
 
+  $(".p-gnav a").click(function () {
+    //ナビゲーションのリンクがクリックされたら
+    $(".js-humburger").removeClass("js-active"); //ボタンの activeクラスを除去し
+    $(".js-gnav").removeClass("js-panelactive"); //ナビゲーションのpanelactiveクラスも除去
+  });
+
+  $('a[href^="#"]').click(function () {
+    var adjust = 0;
+    var speed = 400;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top + adjust;
+    $("body,html").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
 });
